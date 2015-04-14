1de94b3dc505c10e4d7da54d1a0544fbe589d04f
Muhamad Mahfudz <mahfudz@richcoremedia.com>
2015-04-14 22:30:00 +0800

    f

--- DashClass.php
+++ DashClass.php
@@ -3,9 +3,8 @@
 use \Carbon\Carbon;
 
 class UserController extends BaseController {
-	
-	public function postUpdate($id)
-	{
+
+	public function postUpdate($id) {
 		$updateUser = User::find($id);
 		$updateUser->first_name = Input::get('first_name');
 		$updateUser->last_name = Input::get('last_name');
@@ -13,17 +12,19 @@ class UserController extends BaseController {
 
 			if (Input::hasFile('profile_pic')) {
 				$filename = md5(Carbon::now());
-
 				$img = Image::make($_FILES['profile_pic']['tmp_name']);
 				$img->fit(200, 200);
-				$img->save( $filename . '.jpg');
-				
-				User::where('id', $id)->update(array('profile_pic' => $filename.'.jpg'));
+				$img->save($filename . '.jpg');
+
+				User::where('id', $id)->update(
+					array('profile_pic' => $filename . '.jpg'));
 			}
 
-			return Redirect::back()->with('message', 'Profil anda telah dikemaskini');
+			return Redirect::back()
+				->with('message', 'Profil anda telah dikemaskini');
 		}
-		
-		return Redirect::back()->withErrors($updateUser->errors());
+
+		return Redirect::back()
+			->withErrors($updateUser->errors());
 	}
 }
